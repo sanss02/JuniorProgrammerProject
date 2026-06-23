704195769402b740b84f962cd124aea7be4ad585
Sandra P Sieres <sieres.san98@gmail.com>
2026-06-23 13:49:33 -0600

    Actualiza con seccion 3.3 y 3.4, adem s de Challenge 3

--- Assets/Project3/Scripts/RunnerPlayerController.cs
+++ Assets/Project3/Scripts/RunnerPlayerController.cs
@@ -9,21 +9,33 @@ public class RunnerPlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+    private Animator runnerPlayerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource runnerPlayerAudio;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         runnerRb = GetComponent<Rigidbody>();
         jumpAction.Enable();
         Physics.gravity *= gravityModifier;
+        runnerPlayerAnim = GetComponent<Animator>();
+        runnerPlayerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (jumpAction.triggered && isOnGround)
+        if (jumpAction.triggered && isOnGround && !gameOver)
         {
             runnerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            runnerPlayerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            runnerPlayerAudio.PlayOneShot(jumpSound, 1.0f);            
         }
     }
 
@@ -32,10 +44,16 @@ public class RunnerPlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            runnerPlayerAnim.SetBool("Death_b", true);
+            runnerPlayerAnim.SetInteger("DeathType_int",1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            runnerPlayerAudio.PlayOneShot(crashSound, 1.0f);
         }
 
     }
