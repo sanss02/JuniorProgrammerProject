b17873efe05faa5131f9972359fe2fa39f5f5e88
Sandra P Sieres <sieres.san98@gmail.com>
2026-06-24 00:07:24 -0600

    Actualiza con challenge 3 completo

--- Assets/Project3/Challenge 3/Scripts/PlayerControllerX3.cs
+++ Assets/Project3/Challenge 3/Scripts/PlayerControllerX3.cs
@@ -7,6 +7,7 @@ public class PlayerControllerX3 : MonoBehaviour
 
     public float floatForce;
     private float gravityModifier = 1.5f;
+    private float floatLimit = 15f;
     private Rigidbody playerRb;
 
     public InputAction floatAction;
@@ -17,6 +18,7 @@ public class PlayerControllerX3 : MonoBehaviour
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
+    public AudioClip boingSound;
 
 
     // Start is called before the first frame update
@@ -24,6 +26,7 @@ public class PlayerControllerX3 : MonoBehaviour
     {
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
         floatAction.Enable();
 
         // Apply a small upward force at the start of the game
@@ -34,6 +37,11 @@ public class PlayerControllerX3 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y > floatLimit)
+        {
+            transform.position = new Vector3(transform.position.x,floatLimit,transform.position.z);
+            playerRb.linearVelocity = new Vector3(playerRb.linearVelocity.x, 0, playerRb.linearVelocity.z);
+        }
         // While space is pressed and player is low enough, float up
         if (floatAction.IsPressed() && !gameOver)
         {
@@ -51,15 +59,21 @@ public class PlayerControllerX3 : MonoBehaviour
             gameOver = true;
             Debug.Log("Game Over!");
             Destroy(other.gameObject);
+            Destroy(gameObject,2.0f);
         } 
 
         // if player collides with money, fireworks
-        else if (other.gameObject.CompareTag("Money"))
+        else if (other.gameObject.CompareTag("Money") && !gameOver)
         {
             fireworksParticle.Play();
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
 
+        }
+        else if (other.gameObject.CompareTag("Ground") && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * 6, ForceMode.Impulse);
+            playerAudio.PlayOneShot(boingSound, 0.75f);
         }
 
     }
--- Assets/Project3/Challenge 3/_Source_Files/SpinObjectsX3.cs
+++ Assets/Project3/Challenge 3/_Source_Files/SpinObjectsX3.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpinObjectsX : MonoBehaviour
+public class SpinObjectsX3 : MonoBehaviour
 {
     public float spinSpeed;
 
